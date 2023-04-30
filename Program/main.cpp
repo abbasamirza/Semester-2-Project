1cf87eef9327808701e4c3ef1925bee61d2ee978
Abbas Ali Mirza <abbasamirza2@gmail.com>
2023-04-30 09:38:17 +0500

    Major UI Changes, completed filing functionality(login)

--- Program/main.cpp
+++ Program/main.cpp
@@ -2,93 +2,292 @@
 #include <fstream>
 #include <string>
 #include <conio.h>
+#include <windows.h>
+
 using namespace std;
 
+#define NOTEXISTS -1
 #define LOGIN 1
 #define SIGNUP 2
-#define EXIT 0
-
+#define FORGOT 3
+#define EXIT 4
 #define ENTER 13
 #define BACKSPACE 8
+#define BOX 178
 
 /* CLASS DEFINITIONS START HERE */
+class OutputScreen
+{
+    public:
+        HANDLE outScreen;
+        WORD defaultColor;
+        CONSOLE_SCREEN_BUFFER_INFO info;
+        int width;
+
+        OutputScreen()
+        {
+            outScreen = GetStdHandle(STD_OUTPUT_HANDLE);
+            width = maximize_outputWindow();
+            defaultColor = info.wAttributes;
+            SetConsoleTitle("ADMISSION PORTAL");
+        }
+
+        int maximize_outputWindow();
+        void display_welcomeMsg();
+        void display_boxes(int);
+        void display_spaces(int);
+        void display_defaultMessages();
+        void display_loginSuccess();
+} console;
+
 class User
 {
     protected:
         string name, NIC, password;
 
     public:
-        friend bool check_NIC(string);
+        User ()
+        {
+            name = NIC = password = "";
+        }
+
+        void set_name(string name)
+        {
+            this->name = name;
+        }
+
+        void set_NIC(string nic)
+        {
+            this->NIC = nic;
+        }
+
+        void set_password(string pass)
+        {
+            password = pass;
+        }
+
+        string get_name()
+        {
+            return name;
+        }
+
+        string get_NIC()
+        {
+            return NIC;
+        }
+
+        string get_password()
+        {
+            return password;
+        }
+
+        friend long long int check_NIC(string);
 };
 /* CLASS DEFINITIONS END HERE */
 
-
 /* FUNCTION PROTOTYPES START HERE */
-int get_loginOrSignup(void);
-string get_NIC(void);
-string get_name(void);
-string get_pass(void);
-bool check_NIC(string);
+int get_loginOrSignup();
+string get_NIC();
+string get_name();
+string get_pass();
+long long int check_NIC(string);
+bool check_pass(string, int);
+User create_user(string, string, string);
 /* FUNCTION PROTOTYPES END HERE */
 
-
 /* MAIN FUNCTION STARTS HERE */
-int main(void)
+int main()
 {
     /* Variable Declerations */
     string inputNIC, inputName, inputPass;
-    int entry;
+    int entry, wrongPassCount;
+    long long int index;
     ofstream out;
 
     /* Create File If It Does Not Exist */
     out.open("Users.txt", ios::app);
+    out.close();
 
+    console.display_defaultMessages();
     entry = get_loginOrSignup();
+
     if (entry == LOGIN)
     {
+        console.display_defaultMessages();
         inputNIC = get_NIC();
-        check_NIC(inputNIC);
+        index = check_NIC(inputNIC);
+        if (index != NOTEXISTS)
+        {
+            cout << endl << endl;
+            wrongPassCount = 3;
+
+            while (wrongPassCount >= 0)
+            {
+                inputPass = get_pass();
+
+                if (check_pass(inputPass, index))
+                {
+                    console.display_defaultMessages();
+                    console.display_loginSuccess();
+                    break;
+                }
+
+                else
+                {
+                    SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+
+                    if (wrongPassCount == 0)
+                    {
+                        cout << "\n\t\t\t\t\t\t\t\t\tThe password you entered is correct, no more tries left\n";
+                        break;
+                    }
+
+                    else if (wrongPassCount > 1)
+                    {
+                        cout << "\n\t\t\t\t\t\t\t\t\tThe password you entered is correct, you have " << wrongPassCount-- << " tries left\n";
+                    }
+
+                    else
+                    {
+                        cout << "\n\t\t\t\t\t\t\t\t\tThe password you entered is correct, you have " << wrongPassCount-- << " try left\n";
+                    }
+
+                    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                }
+            }
+        }
     }
 
     else if (entry == SIGNUP)
     {
+        console.display_defaultMessages();
         inputNIC = get_NIC();
-        inputName = get_name();
-        inputPass = get_pass();
+
+        if (inputNIC != "NIC Number Not Entered")
+        {
+            index = check_NIC(inputNIC);
+            if (index == NOTEXISTS)
+            {
+                cout << endl << endl;
+                inputName = get_name();
+                cout << endl << endl;
+                inputPass = get_pass();
+                console.display_defaultMessages();
+                User user = create_user(inputName, inputNIC, inputPass);
+            }
+        }
+
+        // else
+        // {
+
+        // }
     }
+
+    _getch();
 
     return 0;
 }
 /* MAIN FUNCTION ENDS HERE */
 
-
 /* FUNCTION DEFINITIONS START HERE */
-int get_loginOrSignup(void)
+int OutputScreen::maximize_outputWindow()
+{
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_SHOWMAXIMIZED);
+
+    if (!outScreen || !GetConsoleScreenBufferInfo(outScreen, &info))
+    {
+        return 80; // Default console width
+    }
+
+    return info.dwSize.X;
+}
+
+void OutputScreen::display_welcomeMsg()
+{
+    display_boxes(33);
+    display_spaces(3);
+    SetConsoleTextAttribute(console.outScreen, FOREGROUND_BLUE);
+    cout << "WELCOME TO ADMISSION PORTAL";
+    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+    display_spaces(3);
+    display_boxes(33);
+}
+
+void OutputScreen::display_boxes(int numOfBoxes)
+{
+    for (int i = 0; i < (console.width - numOfBoxes) / 2; i++)
+    {
+        cout << (char)BOX;
+    }
+}
+
+void OutputScreen::display_spaces(int numOfSpaces)
+{
+    for (int i = 0; i < numOfSpaces; i++)
+    {
+        cout << " ";
+    }
+}
+
+void OutputScreen::display_defaultMessages()
+{
+    system("cls");
+    display_boxes(-174);
+    cout << endl << endl;
+    display_welcomeMsg();
+    cout << endl << endl << endl;
+}
+
+void OutputScreen::display_loginSuccess()
+{
+    string success = "Login Successful!";
+
+    SetConsoleTextAttribute(console.outScreen, FOREGROUND_GREEN);
+    cout << "\t\t\t\t\t\t\t\t\t      ";
+
+    for (int i = 0; i < success.length(); i++)
+    {
+        Sleep(40);
+        cout << success[i];
+    }
+
+    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+}
+
+int get_loginOrSignup()
 {
 	int entry;
 	
-    cout << "Enter the number of the action that you want to perform\n\n" 
-		<< "1. Login\n"
-        << "2. Signup\n"
-        << "0. Exit\n" << endl;
+    cout << "\t\t\t\t\t\t\tEnter the number of the action that you want to perform\n\n" 
+		<< "\t\t\t\t\t\t\t1. Login\n"
+        << "\t\t\t\t\t\t\t2. Signup\n"
+        << "\t\t\t\t\t\t\t3. Forgot Password\n"
+        << "\t\t\t\t\t\t\t4. Exit\n\n"
+        << "\t\t\t\t\t\t\t-> ";
     cin >> entry;
 	cout << endl;
 	
-    while (entry != LOGIN && entry != SIGNUP && entry != EXIT)
+    while (entry != LOGIN && entry != SIGNUP && entry != FORGOT && entry != EXIT)
     {
-        cout << "Invalid option, please re-enter\n";
+        cout << "\t\t\t\t\t\t\tInvalid option, please re-enter\n"
+            << "\t\t\t\t\t\t\t-> ";
         cin >> entry;
     }
     
     return entry;
 }
 
-string get_NIC(void)
+string get_NIC()
 {
     char nic[16];
     int i = 0;
 
-    cout << "Enter NIC number\n";
+    cout << "\t\t\t\t\t\t\t\t\tEnter your NIC number\n"
+        << "\t\t\t\t\t\t\t\t     -> ";
+    SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+    cin.clear();
+
     while (true)
     {
         nic[i] = _getch();
@@ -103,12 +302,16 @@ string get_NIC(void)
 			
 			else if (i == 0)
 			{
-				break;
+				return "NIC Number Not Entered";
 			}
 			
 			else
 			{
-				cout << "\nInvalid NIC number please re-enter\n";
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+				cout << "\n\t\t\t\t\t\t\t\t\tInvalid NIC number please re-enter\n";
+                SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                cout << "\t\t\t\t\t\t\t\t     -> ";
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
 				i = 0;
 			}
         }
@@ -125,30 +328,76 @@ string get_NIC(void)
 		}
     }
 
+    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+
     return nic;
 }
 
-string inputName(void)
+string get_name()
 {
-    string name;
+    char name[31];
+    int i = 0;
 
-    cout << "Enter Name: ";
-    getline(cin >> ws, name);
+    cout << "\t\t\t\t\t\t\t\t\tEnter Your Name\n"
+        << "\t\t\t\t\t\t\t\t     -> ";
+    SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+    cin.clear();
+
+    while (true)
+    {
+        name[i] = _getch();
+
+        if (name[i] == ENTER && i != 0)
+        {
+            if (i > 30)
+            {
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+                cout << "\n\t\t\t\t\t\t\t\t\tName Can Only Be Between 1 and 30 Characters\n";
+                SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                cout << "\t\t\t\t\t\t\t\t     -> ";
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+                i = 0;
+            }
+
+            else
+            {
+                name[i] = '\0';
+                break;
+            }
+        }
+
+        else if (name[i] == BACKSPACE && i > 0)
+        {
+            --i;
+            cout << "\b \b";
+        }
+
+        else if ((isalpha(name[i]) || name[i] == ' ' || name[i] == '-') && i <= 30)
+        {
+            cout << name[i++] << " \b";
+        }
+    }
+    
+    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
 
     return name;
 }
 
-string inputPass(void)
+string get_pass()
 {
-    string pass;
+    char pass[100];
     int i = 0;
 
-    cout << "Enter Password: ";
+    cout << "\t\t\t\t\t\t\t\t\tEnter Password\n"
+        << "\t\t\t\t\t\t\t\t     -> ";
+    SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+    cin.clear();
+
     while (true)
     {
         pass[i] = _getch();
 
-        if (pass[i] == ENTER)
+        if (pass[i] == ENTER) // need to implement security (min characters, uppercase lowercase, numerics)
         {
             pass[i] = '\0';
             break;
@@ -160,18 +409,118 @@ string inputPass(void)
             cout << "\b \b";
         }
 
-        else
+        else if (isdigit(pass[i]) || isalpha(pass[i]) || ispunct(pass[i]))
         {
             ++i;
             cout << "* \b";
         }
     }
+
+    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+
+    return pass;
 }
 
-bool check_NIC(string inputNIC)
+long long int check_NIC(string inputNIC)
 {
-    User* user;
+    ifstream in("Users.txt");
 
+    if (!in)
+    {
+        cout << "File could not open\n";
+        exit(1);
+    }
+
+    int replace;
+    string tempNIC;
+
+    while (getline(in, tempNIC))
+    {
+        replace = 0;
+        for (int i = 1; i < tempNIC.length(); i++)
+        {
+            if (tempNIC[i] == ',')
+            {
+                for (int j = i + 1; j < i + 14; j++)
+                {
+                    if (inputNIC[replace++] != tempNIC[j])
+                    {
+                        replace = 0;
+                        break;
+                    }
+                }
+
+                if (replace != 0)
+                {
+                    return in.tellg();
+                }
+
+                break;
+            }
+        }
+    }
+
+    in.close();
+
+    return NOTEXISTS;
+}
+
+bool check_pass(string inputPass, int index)
+{
+    ifstream in("Users.txt");
+
+    if (!in)
+    {
+        cout << "File could not open\n";
+        exit(2);
+    }
+
+    int check = 0;
+    string tempPass;
+
+    check = inputPass.length() - 1;
+    in.seekg(index - inputPass.length() - 2, ios::beg);
+    getline(in, tempPass);
+
+    if (inputPass == tempPass)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+User create_user(string inputName, string inputNIC, string inputPass)
+{
+    ofstream out("Users.txt", ios::app);
+
+    if (!out)
+    {
+        cout << "File could not open\n";
+        exit(111);
+    }
+
+    User user;
+    string success = "Account Created Successfully!";
+
+    user.set_name(inputName);
+    user.set_NIC(inputNIC);
+    user.set_password(inputPass);
+
+    out << inputName << "," << inputNIC << "," << inputPass << endl;
+    out.close();
+
+    cout << "\t\t\t\t\t\t\t\t        ";
+    SetConsoleTextAttribute(console.outScreen, FOREGROUND_GREEN);
     
+    for (int i = 0; i < success.length(); i++)
+    {
+        Sleep(40);
+        cout << success[i];
+    }
+
+    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+    
+    return user;
 }
 /* FUNCTION DEFINITIONS END HERE */
