c3ae9e83165d26b7cc02f60ae2e0d7e4e8c287c5
Abbas Ali Mirza <abbasamirza2@gmail.com>
2023-05-02 17:53:03 +0500

    Fixed empty password as correct bug, added password checks and invalid condition checks

--- Program/main.cpp
+++ Program/main.cpp
@@ -80,8 +80,6 @@ class User
         {
             return password;
         }
-
-        friend long long int check_NIC(string);
 };
 /* CLASS DEFINITIONS END HERE */
 
@@ -115,46 +113,97 @@ int main()
     {
         console.display_defaultMessages();
         inputNIC = get_NIC();
-        index = check_NIC(inputNIC);
-        if (index != NOTEXISTS)
+        if (inputNIC != "NIC Number Not Entered")
         {
-            cout << endl << endl;
-            wrongPassCount = 3;
-
-            while (wrongPassCount >= 0)
+            index = check_NIC(inputNIC);
+            if (index != NOTEXISTS)
             {
-                inputPass = get_pass();
+                cout << endl << endl;
+                wrongPassCount = 3;
 
-                if (check_pass(inputPass, index))
+                while (wrongPassCount >= 0)
                 {
-                    console.display_defaultMessages();
-                    console.display_loginSuccess();
-                    break;
-                }
+                    inputPass = get_pass();
 
-                else
-                {
-                    SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
-
-                    if (wrongPassCount == 0)
+                    if (check_pass(inputPass, index))
                     {
-                        cout << "\n\t\t\t\t\t\t\t\t\tThe password you entered is correct, no more tries left\n";
+                        console.display_defaultMessages();
+                        console.display_loginSuccess();
                         break;
-                    }
-
-                    else if (wrongPassCount > 1)
-                    {
-                        cout << "\n\t\t\t\t\t\t\t\t\tThe password you entered is correct, you have " << wrongPassCount-- << " tries left\n";
                     }
 
                     else
                     {
-                        cout << "\n\t\t\t\t\t\t\t\t\tThe password you entered is correct, you have " << wrongPassCount-- << " try left\n";
-                    }
+                        SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
 
-                    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                        if (wrongPassCount == 0)
+                        {
+                            cout << "\n\n\t\t\t\t\t\t\t\t\tThe password you entered is incorrect, no more tries left\n";
+                            break;
+                        }
+
+                        else if (wrongPassCount > 1)
+                        {
+                            cout << "\n\n\t\t\t\t\t\t\t\t\tThe password you entered is incorrect, you have " << wrongPassCount-- << " tries left\n";
+                        }
+
+                        else
+                        {
+                            cout << "\n\n\t\t\t\t\t\t\t\t\tThe password you entered is incorrect, you have " << wrongPassCount-- << " try left\n";
+                        }
+
+                        SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                    }
                 }
             }
+
+            else
+            {
+                cout << endl << endl;
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+                cout << "\t\t\t\t\t\t\t\tAccount with the entered NIC Number doesnot exist\n\n"; // 8
+                SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                cout << "\t\t\t\t\t\t\t\t1. Proceed with signup with the entered NIC number\n"
+                    << "\t\t\t\t\t\t\t\t2. Go to main menu\n"
+                    << "\t\t\t\t\t\t\t\t3. Exit\n\n"
+                    << "\t\t\t\t\t\t\t\t-> ";
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+                cin >> entry;
+
+                while (entry != 1 && entry != 2 && entry != 3)
+                {
+                    SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+                    cout << "\t\t\t\t\t\t\t\tInvalid option, please re-enter\n"; // 8
+                    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                    cout << "\t\t\t\t\t\t\t\t-> ";
+                    SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+                    cin >> entry;
+                }
+
+                SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+
+                if (entry == 1)
+                {
+                    cout << endl << endl;
+                    inputName = get_name();
+                    cout << endl << endl;
+                    inputPass = get_pass();
+                    console.display_defaultMessages();
+                    User user = create_user(inputName, inputNIC, inputPass);
+                }
+
+                else if (entry == 2)
+                {
+                    main();
+                }
+            }
+        }
+
+        else
+        {
+            SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+            cout << "\n\n\t\t\t\t\t\t\t\tYou didnot enter a NIC number, exiting program\n\n"; // 8
+            SetConsoleTextAttribute(console.outScreen, console.defaultColor);
         }
     }
 
@@ -175,12 +224,85 @@ int main()
                 console.display_defaultMessages();
                 User user = create_user(inputName, inputNIC, inputPass);
             }
+
+            else
+            {
+                cout << endl << endl;
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+                cout << "\t\t\t\t\t\t\t\tAccount with the entered NIC Number already exists\n\n"; // 8
+                SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                cout << "\t\t\t\t\t\t\t\t1. Proceed with login with the entered NIC number\n"
+                    << "\t\t\t\t\t\t\t\t2. Go to main menu\n"
+                    << "\t\t\t\t\t\t\t\t3. Exit\n\n"
+                    << "\t\t\t\t\t\t\t\t-> ";
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+                cin >> entry;
+
+                while (entry != LOGIN && entry != 2 && entry != 3)
+                {
+                    SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+                    cout << "\t\t\t\t\t\t\t\tInvalid option, please re-enter\n" // 8
+                        << "\t\t\t\t\t\t\t\t-> ";
+                    SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+                    cin >> entry;
+                }
+
+                SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                
+                if (entry == LOGIN)
+                {
+                    cout << endl << endl;
+                    wrongPassCount = 3;
+
+                    while (wrongPassCount >= 0)
+                    {
+                        inputPass = get_pass();
+
+                        if (check_pass(inputPass, index))
+                        {
+                            console.display_defaultMessages();
+                            console.display_loginSuccess();
+                            break;
+                        }
+
+                        else
+                        {
+                            SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+
+                            if (wrongPassCount == 0)
+                            {
+                                cout << "\n\n\t\t\t\t\t\t\t\t\tThe password you entered is incorrect, no more tries left\n";
+                                break;
+                            }
+
+                            else if (wrongPassCount > 1)
+                            {
+                                cout << "\n\n\t\t\t\t\t\t\t\t\tThe password you entered is incorrect, you have " << wrongPassCount-- << " tries left\n";
+                            }
+
+                            else
+                            {
+                                cout << "\n\n\t\t\t\t\t\t\t\t\tThe password you entered is incorrect, you have " << wrongPassCount-- << " try left\n";
+                            }
+
+                            SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                        }
+                    }
+                }
+
+                else if (entry == 2)
+                {
+                    main();
+                }
+            }
         }
 
-        // else
-        // {
-
-        // }
+        else
+        {
+            SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+            cout << "\n\n\t\t\t\t\t\t\t\tYou didnot enter a NIC number, exiting program\n\n"; // 8
+            SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+        }
     }
 
     _getch();
@@ -205,8 +327,8 @@ int OutputScreen::maximize_outputWindow()
 
 void OutputScreen::display_welcomeMsg()
 {
-    display_boxes(33);
-    display_spaces(3);
+    display_boxes(33); // 33
+    display_spaces(3); // 3
     SetConsoleTextAttribute(console.outScreen, FOREGROUND_BLUE);
     cout << "WELCOME TO ADMISSION PORTAL";
     SetConsoleTextAttribute(console.outScreen, console.defaultColor);
@@ -233,7 +355,7 @@ void OutputScreen::display_spaces(int numOfSpaces)
 void OutputScreen::display_defaultMessages()
 {
     system("cls");
-    display_boxes(-174);
+    display_boxes(-174); // -174
     cout << endl << endl;
     display_welcomeMsg();
     cout << endl << endl << endl;
@@ -244,7 +366,7 @@ void OutputScreen::display_loginSuccess()
     string success = "Login Successful!";
 
     SetConsoleTextAttribute(console.outScreen, FOREGROUND_GREEN);
-    cout << "\t\t\t\t\t\t\t\t\t      ";
+    cout << "\t\t\t\t\t\t\t\t\t      "; // 9
 
     for (int i = 0; i < success.length(); i++)
     {
@@ -259,21 +381,27 @@ int get_loginOrSignup()
 {
 	int entry;
 	
-    cout << "\t\t\t\t\t\t\tEnter the number of the action that you want to perform\n\n" 
+    cout << "\t\t\t\t\t\t\tEnter the number of the action that you want to perform\n\n" // 7
 		<< "\t\t\t\t\t\t\t1. Login\n"
         << "\t\t\t\t\t\t\t2. Signup\n"
         << "\t\t\t\t\t\t\t3. Forgot Password\n"
         << "\t\t\t\t\t\t\t4. Exit\n\n"
         << "\t\t\t\t\t\t\t-> ";
+    SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
     cin >> entry;
 	cout << endl;
 	
     while (entry != LOGIN && entry != SIGNUP && entry != FORGOT && entry != EXIT)
     {
-        cout << "\t\t\t\t\t\t\tInvalid option, please re-enter\n"
-            << "\t\t\t\t\t\t\t-> ";
+        SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+        cout << "\t\t\t\t\t\t\tInvalid option, please re-enter\n"; // 7
+        SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+        cout << "\t\t\t\t\t\t\t-> ";
+        SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
         cin >> entry;
     }
+
+    SetConsoleTextAttribute(console.outScreen, console.defaultColor);
     
     return entry;
 }
@@ -283,7 +411,7 @@ string get_NIC()
     char nic[16];
     int i = 0;
 
-    cout << "\t\t\t\t\t\t\t\t\tEnter your NIC number\n"
+    cout << "\t\t\t\t\t\t\t\t\tEnter your NIC number\n" // 9
         << "\t\t\t\t\t\t\t\t     -> ";
     SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
     cin.clear();
@@ -308,7 +436,7 @@ string get_NIC()
 			else
 			{
                 SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
-				cout << "\n\t\t\t\t\t\t\t\t\tInvalid NIC number please re-enter\n";
+				cout << "\n\t\t\t\t\t\t\t\t\tInvalid NIC number please re-enter\n"; // 9
                 SetConsoleTextAttribute(console.outScreen, console.defaultColor);
                 cout << "\t\t\t\t\t\t\t\t     -> ";
                 SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
@@ -338,7 +466,7 @@ string get_name()
     char name[31];
     int i = 0;
 
-    cout << "\t\t\t\t\t\t\t\t\tEnter Your Name\n"
+    cout << "\t\t\t\t\t\t\t\t\tEnter Your Name\n" // 9
         << "\t\t\t\t\t\t\t\t     -> ";
     SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
     cin.clear();
@@ -352,7 +480,7 @@ string get_name()
             if (i > 30)
             {
                 SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
-                cout << "\n\t\t\t\t\t\t\t\t\tName Can Only Be Between 1 and 30 Characters\n";
+                cout << "\n\t\t\t\t\t\t\t\t\tName Can Only Be Between 1 and 30 Characters\n"; // 9
                 SetConsoleTextAttribute(console.outScreen, console.defaultColor);
                 cout << "\t\t\t\t\t\t\t\t     -> ";
                 SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
@@ -385,10 +513,10 @@ string get_name()
 
 string get_pass()
 {
-    char pass[100];
+    char pass[26];
     int i = 0;
 
-    cout << "\t\t\t\t\t\t\t\t\tEnter Password\n"
+    cout << "\t\t\t\t\t\t\t\t\tEnter Password\n" // 9
         << "\t\t\t\t\t\t\t\t     -> ";
     SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
     cin.clear();
@@ -397,10 +525,23 @@ string get_pass()
     {
         pass[i] = _getch();
 
-        if (pass[i] == ENTER) // need to implement security (min characters, uppercase lowercase, numerics)
+        if (pass[i] == ENTER)
         {
-            pass[i] = '\0';
-            break;
+            if (i >= 8 && i <= 24)
+            {
+                pass[i] = '\0';
+                break;
+            }
+            
+            else
+            {
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_RED);
+                cout << "\n\t\t\t\t\t\t\t\t\tPassowrd must be between 8 tou 24 characters\n"; // 9
+                SetConsoleTextAttribute(console.outScreen, console.defaultColor);
+                cout << "\t\t\t\t\t\t\t\t     -> ";
+                SetConsoleTextAttribute(console.outScreen, FOREGROUND_INTENSITY);
+                i = 0;
+            }
         }
 
         else if (pass[i] == BACKSPACE && i > 0)
@@ -475,10 +616,8 @@ bool check_pass(string inputPass, int index)
         exit(2);
     }
 
-    int check = 0;
     string tempPass;
 
-    check = inputPass.length() - 1;
     in.seekg(index - inputPass.length() - 2, ios::beg);
     getline(in, tempPass);
 
@@ -510,7 +649,7 @@ User create_user(string inputName, string inputNIC, string inputPass)
     out << inputName << "," << inputNIC << "," << inputPass << endl;
     out.close();
 
-    cout << "\t\t\t\t\t\t\t\t        ";
+    cout << "\t\t\t\t\t\t\t\t        "; // 8
     SetConsoleTextAttribute(console.outScreen, FOREGROUND_GREEN);
     
     for (int i = 0; i < success.length(); i++)
